90264d57022720f189ae7db34ee5b77f1521f8fe
smantavya <smantavya@gmail.com>
2021-05-22 17:56:21 +0530

    Hidden secret key

--- DjangoPizza/settings.py
+++ DjangoPizza/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+from .sec import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k1tcreqofmx66rjce$!_634jwbtc*z7!cu-h^e#wn@gprk!8ma'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
